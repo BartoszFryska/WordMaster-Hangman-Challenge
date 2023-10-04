09844f3ed8a72e471a46d3998565bfce4eff3dac
BartoszFryska <bartosz.fryska.91@gmail.com>
2023-10-04 23:56:08 +0200

    Started working on an intro

--- InterfaceLookFunctions.cpp
+++ InterfaceLookFunctions.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include <string>
 #include <string.h>
+#include <unistd.h>
+#include <cstring>
+#include <memory>
+#include <stdexcept>
+#include <array>
+#include <cstdio>
+#include <vector>
+#include <cstdlib>
+#include <cstdio>
+#include <fstream>
 
 void PrintIntro() {
 
-    //printing intro
+    system ( "clear" );
+    printf ("                     +@@@@@@@@@@@@@@@@@@@@.\n                    +                   .=\n                    +                .@ ..@\n                    +                =.    +\n                    +                @.    @\n                    *.                @@@@.\n                    @.                  @\n                   .@.                  @.. +@\n                   .@                  .@.\n                    #                   -\n                                       -\n                   :.                 .@.\n                   @                 @...@..\n                   @             ..@.     .@.\n                   @                        .\n                   @\n       .           @\n                   .   .      .. )");
+
+    printf ( "\n\n");
+    sleep ( 1 );
+
+    system ( "figlet -f block \"Hangman\"" );
 }
 
 void PrintExitMessage () {
